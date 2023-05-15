11c46845db7ddafbb0db93099b8392ffc69324ab
John Sudds <jsuddsjr@users.noreply.github.com>
2023-05-14 19:31:55 -0700

    Form validation

---
+++ public/javascripts/form-validation.js
@@ -0,0 +1,28 @@
+;(function () {
+	// Fetch all the forms we want to apply custom Bootstrap validation styles to
+	// eslint-disable-next-line no-undef
+	for (const form of document.querySelectorAll('form.needs-validation')) {
+		form.addEventListener(
+			'submit',
+			(event) => {
+				if (!form.checkValidity()) {
+					event.preventDefault()
+					event.stopPropagation()
+				}
+
+				form.classList.add('was-validated')
+			},
+			false,
+		)
+	}
+
+	// eslint-disable-next-line no-undef
+	for (const button of document.querySelectorAll('button[type="reset"]'))
+		button.addEventListener(
+			'click',
+			() => {
+				button.form.classList.remove('was-validated')
+			},
+			false,
+		)
+})()
